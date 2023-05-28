b2b35b7cce5f6659971c326809c7b7c9c86cade3
ethanauditore <ethan@ciencias.unam.mx>
2023-05-27 19:11:42 -0600

    add Dish model

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Dish(models.Model):
+
+    class Category(models.TextChoices):
+        STARTER = "ST", "Entradas"
+        MAIN_DISH = "MD", "Platos principales"
+        DRINKS = "DR", "Bebidas"
+        DESSERTS = "DE", "Postres"
+        ICE_CREAMS = "IC", "Helados"
+
+    name = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField()
+    description = models.TextField()
+    category = models.CharField(max_length=2, choices=Category.choices)
+    available = models.BooleanField()
+
+    def __str__(self):
+        return self.name
