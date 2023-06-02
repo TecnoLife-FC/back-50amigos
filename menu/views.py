43338c943f08513639a3dfc7c23e02563845c0fe
ethanauditore <ethan@ciencias.unam.mx>
2023-06-01 18:51:03 -0600

    add index view

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Dish
+
+class IndexView(generic.ListView):
+    template_name = "menu/index.html"
+    context_object_name = "dish_list"
+
+    def get_queryset(self):
+        return Dish.objects.all().order_by("name")
